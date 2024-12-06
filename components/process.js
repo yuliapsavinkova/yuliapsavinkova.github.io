d4f6db7368a87b15a3632f0641fdfcc28948e06b
Yulia Savinkova <yuliasavinkova@gmail.com>
2024-12-06 18:00:55 -0500

    mobile fix

--- components/process.js
+++ components/process.js
@@ -1,7 +1,7 @@
 class WorkingProcessComponent extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <section id="working-process"  class="section working-process">
+            <section id="working-process" class="section working-process parallax-background">
                 <h2>Working Process</h2>
                 <h5>Streamlined Steps to Deliver Reliable and Scalable Software Solutions</h5>
                 <div class="gra-separator"></div>
