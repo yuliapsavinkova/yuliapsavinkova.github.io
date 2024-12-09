cfd082ef91c01d7526c53e74da554aa2b3ba9d6a
Yulia Savinkova <yuliasavinkova@gmail.com>
2024-12-09 17:50:04 -0500

    Logo.

--- components/hero.js
+++ components/hero.js
@@ -4,17 +4,19 @@ class HeroComponent extends HTMLElement {
             <section id="hero" class="hero">
                 <div class="overlay"></div>
                 <div class="hero-content">
-                    <social-icons></social-icons>
-                    <div>
+                    <div class="hero-heading">
                         <h1>Yulia Savinkova</h1>
                         <h2>Software Engineer</h2>
                         <h3>10+ years of experience</h3>
                         <h4>MS in Computer Science</h4>
                         <h5>HTML, CSS, JavaScript, React and Node</h5>
                     </div>
+                    <social-icons></social-icons>
                     <a href="#about-me" class="button-link">Learn More</a>
                 </div>
+                <div class="content-float"></div>
             </section>
+            
         `;
     }
 }
--- components/social-icons.js
+++ components/social-icons.js
@@ -2,18 +2,18 @@ class SocialIcons extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <style>
-                .social-icons {
-                    display: flex;
-                    width: fit-content;
-                    gap: 1.5rem;
-                }
+            .social-icons {
+                display: flex;
+                width: fit-content;
+                gap: 1.5rem;
+            }
             </style>
             <div class="social-icons">
                 <a href="https://github.com/yuliapsavinkova" target="_blank"><i class="fab fa-github fa-xl"></i></a>
-                <a href="https://www.linkedin.com/in/juliia/" target="_blank"><i class="fab fa-linkedin fa-xl"></i></a>
+                <a href="https://www.linkedin.com/in/juliia" target="_blank"><i class="fab fa-linkedin fa-xl"></i></a>
                 <a href="https://codepen.io/star5/pens/public" target="_blank"><i class="fab fa-codepen fa-xl"></i></a>
-                <a href="#"><i class="fa-solid fa-blog fa-xl"></i></a>
-            <div>
+                <a href="https://codepen.io/star5/pens/public" target="_blank"><i class="fa-solid fa-blog fa-xl"></i></a>
+            </div>
         `;
     }
 }
