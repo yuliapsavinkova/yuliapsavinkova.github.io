1dd4b1e61fc05a61c54b9855bdb89e5abaaea54e
Yulia Savinkova <yuliasavinkova@gmail.com>
2024-12-28 12:52:15 -0500

    Menu

--- components/header/hamburger.js
+++ components/header/hamburger.js
@@ -1,57 +1,51 @@
+// TODO: handle of toggle open case.
 class HamburgerComponent extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = `
           <div id="hamburger" class="hamburger">
-              <div id="nav-btn" class="nav-btn">
-                  <i class="fa-solid fa-bars fa-2x hamburger-bars"></i>
-                  <i class="fa-solid fa-xmark fa-2x hamburger-x"></i>
-              </div>
-              <ul class="nav-menu animate-menu">
+              <input type="checkbox" id="menu-toggle" class="menu-checkbox">
+              <label for="menu-toggle" class="menu-icon">☰</label>
+              <nav class="menu-list menu-animate">
+                <ul>
                   <li><a href="./index.html">Home</a></li>
                   <li><a href="./about.html">About</a></li>
                   <li><a href="./work.html">Work</a></li>
                   <li><a href="./index.html#working-process">Blog</a></li>
                   <li><a href="./contact.html">Contact</a></li>
-              </ul>
+                </ul>
+              </nav>
           </div>
         `;
 
-    this.menuButton = document.getElementById("nav-btn");
-    this._handleMenuClick = this._handleMenuClick.bind(this);
+    this.checkbox = document.getElementById("menu-toggle");
     this._handleResize = this._handleResize.bind(this);
     this._handleScroll = this._handleScroll.bind(this);
     document.addEventListener("click", (e) => this._handleOutsideClick(e));
   }
 
-  _handleMenuClick() {
-    this.classList.toggle("open");
-  }
-
   _handleOutsideClick(event) {
     if (!this.contains(event.target)) {
-      this.classList.remove("open");
+      this.checkbox.checked = false;
     }
   }
 
   _handleResize() {
-    this.classList.remove("open");
+    this.checkbox.checked = false;
   }
 
   _handleScroll() {
-    this.classList.remove("open");
+    this.checkbox.checked = false;
   }
 
   connectedCallback() {
     window.addEventListener("resize", Utils.throttle(this._handleResize, 200));
     window.addEventListener("scroll", Utils.throttle(this._handleScroll, 300));
-    this.menuButton.addEventListener("click", this._handleMenuClick);
   }
 
   disconnectedCallback() {
     window.removeEventListener("resize", this._handleResize);
     window.removeEventListener("scroll", this._handleScroll);
-    this.menuButton.removeEventListener("click", this._handleMenuClick);
   }
 }
 
