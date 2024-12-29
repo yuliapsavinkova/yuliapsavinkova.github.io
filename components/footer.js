b109611f159afaeee8f4bab2f6c15398c2f84157
Yulia Savinkova <yuliasavinkova@gmail.com>
2024-12-28 20:37:07 -0500

    Debug

--- components/footer.js
+++ components/footer.js
@@ -1,13 +1,13 @@
 class FooterComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-            <footer id="footer" class="footer">
-                <social-icons></social-icons>
-                <div class="footer-copyright">
-                    <p>&copy; 2024 Yulia Savinkova</p>
-                </div>
-            </footer>
-        `;
+        <footer id="footer" class="footer">
+            <social-icons></social-icons>
+            <div class="footer-copyright">
+                <p>&copy; 2024 Yulia Savinkova</p>
+            </div>
+        </footer>
+    `;
   }
 }
 customElements.define("footer-component", FooterComponent);
