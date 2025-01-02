bc34648209740ed9e7348e9fcef8589269d3268e
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-01-02 16:34:20 -0500

    Footer

--- portfolio/components/footer.js
+++ portfolio/components/footer.js
@@ -1,10 +1,12 @@
 class FooterComponent extends HTMLElement {
   connectedCallback() {
+    const copyrightName = this.getAttribute("copyright-name") || "All rights reserved.";
+    const year = new Date().getFullYear(); // Get current year
     this.innerHTML = `
         <footer id="footer" class="footer">
             <social-icons></social-icons>
             <div class="footer-copyright">
-                <p>&copy; 2024 Yulia Savinkova</p>
+                <p>&copy; ${year} ${copyrightName}</p>
             </div>
         </footer>
     `;
