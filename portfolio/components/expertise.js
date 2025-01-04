53e6d28f7b8a2e18807f49da05701e8cde02ae31
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-01-04 16:40:41 -0500

    Links.

--- portfolio/components/expertise.js
+++ portfolio/components/expertise.js
@@ -8,24 +8,24 @@ class ExpertiseComponent extends HTMLElement {
                 <div class="expertise-cards-container">
                     <div class="service-card">
                         <h4>App Development</h4>
-                        <img src="assets/images/web-development-pro.svg" class="work-image" alt="web-development-image" />            
+                        <img src="../../assets/images/web-development-pro.svg" class="work-image" alt="web-development-image" />            
                         <p>Build applications using modern frameworks such as React and Angular.</p>
                         <a href="#expertise"><i class="fa-regular fa-circle-right fa-xl"></i></a>
                     </div>
                     <div class="service-card">
                         <h4>Web Development</h4>
-                        <img src="assets/images/maintenance-pro.svg" class="work-image" alt="maintenance-image" />
+                        <img src="../../assets/images/maintenance-pro.svg" class="work-image" alt="maintenance-image" />
                         <p>Build websites from scratch using HTML, CSS, JavaScript, and Web Components.</p>
                         <a href="#expertise"><i class="fa-regular fa-circle-right fa-xl"></i></a>
                     </div>
                     <div class="service-card">
                         <h4>UI/UX Design</h4>
-                        <img src="assets/images/design-pro.svg" class="work-image" alt="design-image" />
+                        <img src="../../assets/images/design-pro.svg" class="work-image" alt="design-image" />
                         <p>Building responsive layouts that work across all devices (desktop, tablet, mobile).</p>
                         <a href="#expertise"><i class="fa-regular fa-circle-right fa-xl"></i></a>
                     </div>
                 </div>
-                <a href="../work.html" class="button-link">Learn More</a>
+                <a href="../portfolio/work.html" class="button-link">Learn More</a>
             </section>
         `;
   }
