4f4aec9c3e7cad27ec347b689edb127174ed0797
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-02-26 13:37:07 -0500

    Updates

--- assets/index-B2KgxHzX.js
+++ assets/index-B2KgxHzX.js
@@ -44,7 +44,7 @@
         <div class="arrow"><i class="fa-duotone fa-solid fa-angle-up"></i></div>
       </div>
     `+this.innerHTML,this.progressContainer=document.querySelector("#progress-container"),this.progressRing=document.querySelector("#progress-ring"),this.updateProgress=this.updateProgress.bind(this),this.scrollToTop=this.scrollToTop.bind(this)}connectedCallback(){window.addEventListener("scroll",this.updateProgress),this.progressContainer.addEventListener("click",this.scrollToTop),this.updateProgress()}disconnectedCallback(){window.removeEventListener("scroll",this.updateProgress),this.progressContainer.removeEventListener("click",this.scrollToTop)}updateProgress(){const t=window.scrollY,n=document.documentElement.scrollHeight-window.innerHeight,s=Math.min(t/n*100,100);t>0?this.progressContainer.classList.add("visible"):this.progressContainer.classList.remove("visible"),this.progressRing.style.setProperty("--scroll-progress",`${s}%`)}scrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}}customElements.define("scroll-progress-ring",b);class v extends HTMLElement{connectedCallback(){this.innerHTML=`
-            <section id="hero" class="hero bg-laptop bg-texture">
+            <section id="hero" class="hero bg-laptop-with-texture">
                 <div class="hero-portrait">
                     <img src="./images/hero-background.svg" alt="Portrait - photo of Yulia Savinkova."/>
                 </div>
@@ -165,115 +165,111 @@
             </section>
         `}}customElements.define("about-component",k);class y extends HTMLElement{connectedCallback(){this.innerHTML=`
               <section id="work-experience" class="section work-experience">
-                  <section-header 
-                      title="Work Experience"
-                      sub-title="A Journey of Growth">
-                  </section-header>
+                <section-header 
+                    title="Work Experience"
+                    sub-title="A Journey of Growth">
+                </section-header>
 
-                  <div class="work-experience-container">
-                    <div class="work-experience-content">
-                        <h5>Founder & Freelance Web Developer, Gravity Workflow, 2019 - Present</h5>
-                        <ul>
-                          <li>Founded and independently manage a freelance web development business with a focus on building long-term client relationships and business growth.</li>
-                          <li>Specialize in developing custom websites and front-end web applications using HTML, CSS, JavaScript, React, Angular, and Node.js.</li>
-                          <li>Provide end-to-end solutions, from project scoping and design to development and deployment.</li>
-                          <li>Handle all aspects of business operations, including client acquisition, project management, and marketing.</li>
-                          <li>Aim to expand and scale the business by building a strong brand and long-term partnerships with clients.</li>
-                          <!--<li>Create custom HTML web components and reusable shared libraries.</li>
-                          <li>Build responsive layouts that work across all screen sizes.</li>
-                          <li>Modernize existing applications by rebuilding them with modern technologies.</li>-->
-                        </ul>
-                        <div class="work-experience-technologies">
-                          <span>JavaScript</span>
-                          <span>Web Components</span>
-                          <span>React</span>
-                          <span>Three.js</span>
-                          <span>Node.js</span>
-                          <span>CSS</span>
-                          <span>HTML</span>
-                        </div>
-                    </div>
-
-                    <div class="work-experience-content">
-                        <h5>Sr. Software Engineer, Bloomberg, New York, NY 2016 - 2019</h5>
-                        <ul>
-                          <li>Worked on developing bloomberg add-ins for Excel, Word and Outlook.</li>
-                          <li>Designed and developed a research management tool.</li>
-                          <li>Conducted technical interviews to evaluate candidates expertise in JavaScript.</li>
-                        </ul>
-                        <div class="work-experience-technologies">
-                          <span>TypeScript</span>
-                          <span>React</span>
-                          <span>Angular</span>
-                          <span>Redux</span>
-                          <span>Webpack</span>
-                          <span>SASS</span>
-                          <span>HTML</span>
-                        </div>
-                    </div>
-
-                    <div class="work-experience-content">
-                        <h5>Sr. UI Developer, Yahoo, San Francisco, CA 2015 - 2016</h5>
-                        <ul>
-                          <li>Rebuilt analytics platform UI for new design.</li>
-                          <li>Developed new features for the analytics platform.</li>
-                        </ul>
-                        <div class="work-experience-technologies">
-                          <span>JavaScript</span>
-                          <span>Angular</span>
-                          <span>D3.js</span>
-                          <span>Node.js</span>
-                          <span>Gulp</span>
-                          <span>Git</span>
-                          <span>SASS</span>
-                          <span>HTML</span>
-                        </div>
-                    </div>
-
-                    <div class="work-experience-content">
-                        <h5>Sr. JavaScript UI Engineer, MuleSoft, San Francisco, CA 2012 - 2015</h5>
-                        <ul>
-                          <li>Developed UI's for CloudHub and Anypoint Platform.</li>
-                          <li>Unified and developed common UI components for all MuleSoft products.</li>
-                        </ul>
-                        <div class="work-experience-technologies">
-                          <span>JavaScript</span>
-                          <span>Angular</span>
-                          <span>Backbone</span>
-                          <span>Node.js</span>
-                          <span>Grunt</span>
-                          <span>Git</span>
-                          <span>LESS</span>
-                          <span>HTML</span>
-                        </div>
-                    </div>
-                  </div>
-
-                  <!--<h2>Education</h2>
-                  <div class="gra-separator"></div>
+                <div class="work-experience-container">
                   <div class="work-experience-content">
-                      <div class="work-experience-header">
-                        Master of Science in Computer Science, Suffolk University, Boston, MA May, 2011
-                      </div>
-                  </div>-->
-
-                  <!--<h2>Other</h2>
-                  <div class="gra-separator"></div>
-
-                  <div class="work-experience-content">
-                      <h5>Instructor, Suffolk University; Boston, MA 2010 - 2011</h5>
+                      <h5>Founder & Freelance Web Developer, Gravity Workflow, 2019 - Present</h5>
                       <ul>
-                        <li>Teach Pre Calculus course.</li>
-                        <li>Teach Web Development course.</li>
+                        <li>Founded and independently manage a freelance web development business with a focus on building long-term client relationships and business growth.</li>
+                        <li>Specialize in developing custom websites and front-end web applications using HTML, CSS, JavaScript, React, Angular, and Node.js.</li>
+                        <li>Provide end-to-end solutions, from project scoping and design to development and deployment.</li>
+                        <li>Handle all aspects of business operations, including client acquisition, project management, and marketing.</li>
+                        <li>Aim to expand and scale the business by building a strong brand and long-term partnerships with clients.</li>
+                        <!--<li>Create custom HTML web components and reusable shared libraries.</li>
+                        <li>Build responsive layouts that work across all screen sizes.</li>
+                        <li>Modernize existing applications by rebuilding them with modern technologies.</li>-->
                       </ul>
+                      <div class="work-experience-technologies">
+                        <span>JavaScript</span>
+                        <span>Web Components</span>
+                        <span>React</span>
+                        <span>Three.js</span>
+                        <span>Node.js</span>
+                        <span>CSS</span>
+                        <span>HTML</span>
+                      </div>
                   </div>
-                  <div class="work-experience-content">
-                      <h5>Passed CFA Level 1, CFA Institute; December, 2017</h5>
-                  </div>-->
 
+                  <div class="work-experience-content">
+                      <h5>Sr. Software Engineer, Bloomberg, New York, NY 2016 - 2019</h5>
+                      <ul>
+                        <li>Worked on developing bloomberg add-ins for Excel, Word and Outlook.</li>
+                        <li>Designed and developed a research management tool.</li>
+                        <li>Conducted technical interviews to evaluate candidates expertise in JavaScript.</li>
+                      </ul>
+                      <div class="work-experience-technologies">
+                        <span>TypeScript</span>
+                        <span>React</span>
+                        <span>Angular</span>
+                        <span>Redux</span>
+                        <span>Webpack</span>
+                        <span>SASS</span>
+                        <span>HTML</span>
+                      </div>
+                  </div>
+
+                  <div class="work-experience-content">
+                      <h5>Sr. UI Developer, Yahoo, San Francisco, CA 2015 - 2016</h5>
+                      <ul>
+                        <li>Rebuilt analytics platform UI for new design.</li>
+                        <li>Developed new features for the analytics platform.</li>
+                      </ul>
+                      <div class="work-experience-technologies">
+                        <span>JavaScript</span>
+                        <span>Angular</span>
+                        <span>D3.js</span>
+                        <span>Node.js</span>
+                        <span>Gulp</span>
+                        <span>Git</span>
+                        <span>SASS</span>
+                        <span>HTML</span>
+                      </div>
+                  </div>
+
+                  <div class="work-experience-content">
+                      <h5>Sr. JavaScript UI Engineer, MuleSoft, San Francisco, CA 2012 - 2015</h5>
+                      <ul>
+                        <li>Developed UI's for CloudHub and Anypoint Platform.</li>
+                        <li>Unified and developed common UI components for all MuleSoft products.</li>
+                      </ul>
+                      <div class="work-experience-technologies">
+                        <span>JavaScript</span>
+                        <span>Angular</span>
+                        <span>Backbone</span>
+                        <span>Node.js</span>
+                        <span>Grunt</span>
+                        <span>Git</span>
+                        <span>LESS</span>
+                        <span>HTML</span>
+                      </div>
+                  </div>
+                </div>
+
+                <!--<h2>Education</h2>
+                <div class="work-experience-content">
+                    <div class="work-experience-header">
+                      Master of Science in Computer Science, Suffolk University, Boston, MA May, 2011
+                    </div>
+                </div>-->
+
+                <!--<h2>Other</h2>
+                <div class="work-experience-content">
+                    <h5>Instructor, Suffolk University; Boston, MA 2010 - 2011</h5>
+                    <ul>
+                      <li>Teach Pre Calculus course.</li>
+                      <li>Teach Web Development course.</li>
+                    </ul>
+                </div>
+                <div class="work-experience-content">
+                    <h5>Passed CFA Level 1, CFA Institute; December, 2017</h5>
+                </div>-->
               </section>
           `}}customElements.define("work-experience-component",y);class x extends HTMLElement{connectedCallback(){this.innerHTML=`
-            <section id="working-process" class="section working-process">
+            <section id="working-process" class="section working-process bg-working-girl">
                 <section-header 
                     title="Working Process"
                     sub-title="Collaborate, Design, Code, and Optimize for Responsive, High-Quality Web Experiences">
