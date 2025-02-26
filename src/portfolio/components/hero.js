6aaeab41d64ce7138186cd5a73a4066bfb23eba7
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-02-26 13:36:55 -0500

    Backgrounds

--- src/portfolio/components/hero.js
+++ src/portfolio/components/hero.js
@@ -1,7 +1,7 @@
 class HeroComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-            <section id="hero" class="hero bg-laptop bg-texture">
+            <section id="hero" class="hero bg-laptop-with-texture">
                 <div class="hero-portrait">
                     <img src="./images/hero-background.svg" alt="Portrait - photo of Yulia Savinkova."/>
                 </div>
--- src/portfolio/components/process.js
+++ src/portfolio/components/process.js
@@ -1,7 +1,7 @@
 class WorkingProcessComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-            <section id="working-process" class="section working-process">
+            <section id="working-process" class="section working-process bg-working-girl">
                 <section-header 
                     title="Working Process"
                     sub-title="Collaborate, Design, Code, and Optimize for Responsive, High-Quality Web Experiences">
