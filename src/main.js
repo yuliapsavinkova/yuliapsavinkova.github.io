e495c507606b4aeaa7c7ff8747592604efa2de0b
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-02-27 13:15:32 -0500

    Cleanup

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// import "./style.css";
-
 import "./shared/js/utils.js";
 import "./shared/components/404/404.js";
 import "./shared/components/header/header.js";
@@ -8,14 +6,15 @@ import "./shared/components/progress/progress.js";
 import "./shared/components/social-icons.js";
 import "./shared/components/debug-panel.js";
 
+import "./portfolio/components/section-header.js";
+
 import "./portfolio/components/hero.js";
+import "./portfolio/components/process.js";
 import "./portfolio/components/expertise.js";
 import "./portfolio/components/expertise-full.js";
 import "./portfolio/components/about.js";
 import "./portfolio/components/work-experience.js";
-import "./portfolio/components/process.js";
 import "./portfolio/components/contact.js";
-import "./portfolio/components/section-header.js";
 
 // main.js (entry point for SPA using Vanilla JS with Vite)
 import { renderPage } from "./router.js";
--- src/portfolio/components/process.js
+++ src/portfolio/components/process.js
@@ -1,13 +1,13 @@
 class WorkingProcessComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-            <section id="working-process" class="section working-process bg-working-girl">
-                <section-header 
-                    title="Working Process"
-                    sub-title="Collaborate, Design, Code, and Optimize for Responsive, High-Quality Web Experiences">
-                </section-header>
-            </section>
-        `;
+      <section id="working-process" class="section working-process bg-working-girl">
+        <section-header 
+          title="Working Process"
+          sub-title="Collaborate, Design, Code, and Optimize for Responsive, High-Quality Web Experiences">
+        </section-header>
+      </section>
+    `;
   }
 }
 customElements.define("working-process-component", WorkingProcessComponent);
