0c5349b8da4cf7e9a96ba1708f7108f15078e5f5
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-02-27 19:14:12 -0500

    Updates

--- assets/index-DTKfZ1vI.js
+++ assets/index-DTKfZ1vI.js
@@ -50,8 +50,8 @@
           position: fixed;
           bottom: 0;
           left: 0;
-          margin: 0.7rem;
-          padding: 0.7rem;
+          margin: 1rem;
+          padding: 1rem;
           border-radius: 1.4rem;
           background: hsl(var(--accent-light-color) / 0.9);
           backdrop-filter: blur(1rem);
@@ -150,31 +150,33 @@
                 sub-title="Transforming Ideas into Code">
             </section-header>
 
-            <div id="work-row-web" class="magic-row">
-                <div class="magic-description">
-                    <h4>Web Development</h4>  
-                    <p>Need a sleek, high-performing website to showcase your brand? I specialize in building modern, responsive websites that look great on any device and deliver a seamless user experience. Whether you need a portfolio, business site, or landing page, I provide custom solutions tailored to your needs. With a focus on speed, SEO, and user-friendly design, I’ll help create a site that not only looks amazing but also drives results. Let's work together to make your online presence shine!</p>
-                    <a href="/contact" class="button button-secondary">Get a Quote</a>
+            <div class="magic-row-container">
+                <div id="work-row-web" class="magic-row">
+                    <div class="magic-description">
+                        <h4>Web Development</h4>  
+                        <p>Need a sleek, high-performing website to showcase your brand? I specialize in building modern, responsive websites that look great on any device and deliver a seamless user experience. Whether you need a portfolio, business site, or landing page, I provide custom solutions tailored to your needs. With a focus on speed, SEO, and user-friendly design, I’ll help create a site that not only looks amazing but also drives results. Let's work together to make your online presence shine!</p>
+                        <a href="/contact" class="button button-secondary">Get a Quote</a>
+                    </div>
+                    <img src="./images/web-dev.png" class="magic-image vertical" alt="maintenance-image" />
                 </div>
-                <img src="./images/web-dev.png" class="magic-image vertical" alt="maintenance-image" />
-            </div>
 
-            <div id="work-row-app" class="magic-row">
-                <img src="./images/app-dev.png" class="magic-image" alt="web-development-image" />            
-                <div class="magic-description">
-                    <h4>App Development</h4>
-                    <p>Need a powerful, high-performance web application? I build dynamic, user-friendly apps using cutting-edge frameworks like React and Angular. Whether it's a business tool, an e-commerce platform, or a custom solution, I create fast, scalable, and secure applications designed to elevate your brand and streamline your operations. Let’s bring your vision to life!</p>
-                    <a href="/contact" class="button button-secondary">Get a Quote</a>
+                <div id="work-row-app" class="magic-row">
+                    <img src="./images/app-dev.png" class="magic-image" alt="web-development-image" />            
+                    <div class="magic-description">
+                        <h4>App Development</h4>
+                        <p>Need a powerful, high-performance web application? I build dynamic, user-friendly apps using cutting-edge frameworks like React and Angular. Whether it's a business tool, an e-commerce platform, or a custom solution, I create fast, scalable, and secure applications designed to elevate your brand and streamline your operations. Let’s bring your vision to life!</p>
+                        <a href="/contact" class="button button-secondary">Get a Quote</a>
+                    </div>
                 </div>
-            </div>
 
-            <div id="work-row-3d" class="magic-row">
-                <div class="magic-description">
-                    <h4>3D Development</h4>  
-                    <p>Bring your website to life with stunning 3D experiences! I create interactive, immersive visuals that run smoothly in any modern browser—perfect for showcasing products, engaging users, or adding a unique touch to your site. Whether you need an interactive model, an animated scene, or a full 3D experience, I’ll build a solution that captivates your audience. Let’s make your vision a reality!</p>  
-                    <a href="/contact" class="button button-secondary">Get a Quote</a>
+                <div id="work-row-3d" class="magic-row">
+                    <div class="magic-description">
+                        <h4>3D Development</h4>  
+                        <p>Bring your website to life with stunning 3D experiences! I create interactive, immersive visuals that run smoothly in any modern browser—perfect for showcasing products, engaging users, or adding a unique touch to your site. Whether you need an interactive model, an animated scene, or a full 3D experience, I’ll build a solution that captivates your audience. Let’s make your vision a reality!</p>  
+                        <a href="/contact" class="button button-secondary">Get a Quote</a>
+                    </div>
+                    <img src="./images/three-d-dev.png" class="magic-image" alt="design-image" />
                 </div>
-                <img src="./images/three-d-dev.png" class="magic-image" alt="design-image" />
             </div>
         </section>
     `}}customElements.define("expertise-full-component",x);class L extends HTMLElement{constructor(){super(),this.innerHTML=`
@@ -232,6 +234,7 @@
                         <span>Node.js</span>
                         <span>CSS</span>
                         <span>HTML</span>
+                        <span>Vite</span>
                       </div>
                   </div>
 
@@ -247,9 +250,9 @@
                         <span>React</span>
                         <span>Angular</span>
                         <span>Redux</span>
-                        <span>Webpack</span>
                         <span>SASS</span>
                         <span>HTML</span>
+                        <span>Webpack</span>
                       </div>
                   </div>
 
@@ -318,15 +321,15 @@
                 <div id="contact-form" class="contact-form">
                     <h3>Contact Me</h3>
                     <form action="https://formspree.io/f/xqakdrpw" method="POST">
-                    <label for="name">Name</label>
-                    <input type="text" autocomplete="on" id="name" name="name" placeholder="Enter your name" required>
+                        <label for="name">Name</label>
+                        <input type="text" autocomplete="on" id="name" name="name" placeholder="Enter your name" required>
 
-                    <label for="email">Email</label>
-                    <input type="email" autocomplete="on" id="email" name="email" placeholder="Enter email address" required>
+                        <label for="email">Email</label>
+                        <input type="email" autocomplete="on" id="email" name="email" placeholder="Enter email address" required>
 
-                    <label for="message">Message</label>
-                    <textarea id="message" name="message" rows="4" placeholder="Enter your Message" required></textarea>
-                    <button type="submit" class="button button-secondary">Send Message</button>
+                        <label for="message">Message</label>
+                        <textarea id="message" name="message" rows="4" placeholder="Enter your Message" required></textarea>
+                        <button type="submit" class="button button-secondary">Send Message</button>
                     </form>
                 </div>
             </section>
