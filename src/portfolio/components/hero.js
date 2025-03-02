3395185298bbc9ec542e0b18cbc9ff2764fd9d6c
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-03-02 10:14:08 -0500

    Particles to hero only

--- src/portfolio/components/hero.js
+++ src/portfolio/components/hero.js
@@ -1,29 +1,53 @@
 class HeroComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = this.getTemplate();
+    this.loadParticles();
   }
 
   getTemplate() {
     return `
       <section id="hero" class="hero bg-texture">
-          <div class="hero-container">
-            <div class="hero-portrait">  
-                <img src="./images/hero-background.svg" alt="Portrait - photo of Yulia Savinkova." />
-            </div>
-            <social-icons></social-icons>
-            <div class="hero-heading">
-                <h1>Yulia Savinkova</h1>
-                <h2>Software Engineer</h2>
-                <h3>10+ years of experience</h3>
-                <h4>MS in Computer Science</h4>
-            </div>
-            <div>
-                <a href="/about" class="button button-primary">Learn More</a>
-                <a href="/contact" class="button button-action">Contact</a>
-            </div>
+        <div id="particles-js"></div>
+        <div class="hero-container">
+          <div class="hero-portrait">  
+            <img src="./images/hero-background.svg" alt="Portrait - photo of Yulia Savinkova." />
           </div>
+          <social-icons></social-icons>
+          <div class="hero-heading">
+            <h1>Yulia Savinkova</h1>
+            <h2>Software Engineer</h2>
+            <h3>10+ years of experience</h3>
+            <h4>MS in Computer Science</h4>
+          </div>
+          <div>
+            <a href="/about" class="button button-primary">Learn More</a>
+            <a href="/contact" class="button button-action">Contact</a>
+          </div>
+        </div>
       </section>
     `;
+  }
+
+  loadParticles() {
+    if (!window.particlesJS) {
+      // Create a script element for particles.js
+      const script = document.createElement("script");
+      script.src = "https://cdn.jsdelivr.net/npm/particles.js";
+      script.onload = () => {
+        this.initParticles();
+      };
+      document.body.appendChild(script);
+    } else {
+      this.initParticles();
+    }
+  }
+
+  initParticles() {
+    import("../../portfolio/components/particles.js")
+      .then(({ particlesConfig }) => {
+        particlesJS("particles-js", particlesConfig);
+      })
+      .catch((err) => console.error("Particles.js config loading failed", err));
   }
 }
 
--- src/portfolio/components/particles.js
+++ src/portfolio/components/particles.js
@@ -1,110 +1,55 @@
 export const particlesConfig = {
   particles: {
-    number: {
-      value: 40,
-      density: {
-        enable: true,
-        value_area: 200,
-      },
-    },
-    color: {
-      value: "#2d4a77",
-    },
+    number: { value: 5, density: { enable: true, value_area: 800 } },
+    color: { value: "#2d4a77" },
     shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#2d4a77",
-      },
-      polygon: {
-        nb_sides: 5,
-      },
-      image: {
-        src: "img/github.svg",
-        width: 100,
-        height: 100,
-      },
+      type: "polygon",
+      stroke: { width: 0, color: "#000" },
+      polygon: { nb_sides: 6 },
+      // image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
-      value: 0.5,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
+      value: 0.1,
+      random: true,
+      anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
-      value: 3,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false,
-      },
+      value: 200,
+      random: false,
+      anim: { enable: true, speed: 10, size_min: 50, sync: false },
     },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#2d4a77",
-      opacity: 0.5,
-      width: 1,
-    },
+    // line_linked: {
+    //   enable: false,
+    //   distance: 200,
+    //   color: "#ffffff",
+    //   opacity: 1,
+    //   width: 2,
+    // },
     move: {
       enable: true,
-      speed: 3,
+      speed: 8,
       direction: "none",
       random: false,
       straight: false,
       out_mode: "out",
       bounce: false,
-      attract: {
-        enable: false,
-        rotateX: 600,
-        rotateY: 1200,
-      },
+      attract: { enable: false, rotateX: 600, rotateY: 1200 },
     },
   },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "grab",
-      },
-      onclick: {
-        enable: true,
-        mode: "push",
-      },
-      resize: true,
-    },
-    modes: {
-      grab: {
-        distance: 140,
-        line_linked: {
-          opacity: 1,
-        },
-      },
-      bubble: {
-        distance: 400,
-        size: 40,
-        duration: 2,
-        opacity: 8,
-        speed: 3,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-      push: {
-        particles_nb: 4,
-      },
-      remove: {
-        particles_nb: 2,
-      },
-    },
-  },
+  // interactivity: {
+  //   detect_on: "canvas",
+  //   events: {
+  //     onhover: { enable: false, mode: "grab" },
+  //     onclick: { enable: false, mode: "push" },
+  //     resize: true,
+  //   },
+  //   modes: {
+  //     grab: { distance: 400, line_linked: { opacity: 1 } },
+  //     bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+  //     repulse: { distance: 200, duration: 0.4 },
+  //     push: { particles_nb: 4 },
+  //     remove: { particles_nb: 2 },
+  //   },
+  // },
   retina_detect: true,
 };
