b4a9736211a015b06701a7ad0cae51d3bfef63c6
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-03-02 08:15:26 -0500

    Updates

--- assets/index-Bj4MvkW0.js
+++ assets/index-Bj4MvkW0.js
@@ -80,19 +80,21 @@
       </div>
     `}}customElements.define("section-header",f);class w extends HTMLElement{connectedCallback(){this.innerHTML=this.getTemplate()}getTemplate(){return`
         <section id="hero" class="hero bg-texture">
-            <div class="hero-portrait">  
-                <img src="./images/hero-background.svg" alt="Portrait - photo of Yulia Savinkova." />
-            </div>
-            <social-icons></social-icons>
-            <div class="hero-heading">
-                <h1>Yulia Savinkova</h1>
-                <h2>Software Engineer</h2>
-                <h3>10+ years of experience</h3>
-                <h4>MS in Computer Science</h4>
-            </div>
-            <div>
-                <a href="/about" class="button button-primary">Learn More</a>
-                <a href="/contact" class="button button-action">Contact</a>
+            <div class="hero-container">
+              <div class="hero-portrait">  
+                  <img src="./images/hero-background.svg" alt="Portrait - photo of Yulia Savinkova." />
+              </div>
+              <social-icons></social-icons>
+              <div class="hero-heading">
+                  <h1>Yulia Savinkova</h1>
+                  <h2>Software Engineer</h2>
+                  <h3>10+ years of experience</h3>
+                  <h4>MS in Computer Science</h4>
+              </div>
+              <div>
+                  <a href="/about" class="button button-primary">Learn More</a>
+                  <a href="/contact" class="button button-action">Contact</a>
+              </div>
             </div>
         </section>
       `}}customElements.define("hero-component",w);class k extends HTMLElement{connectedCallback(){this.innerHTML=`
