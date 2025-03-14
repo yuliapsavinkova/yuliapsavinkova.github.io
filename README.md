2940fe8a8612b9feafb556a55284fa2eb0adb9c6
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-03-14 13:44:06 -0400

    Readme

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # yuliapsavinkova.github.io
 
-Portfolio
+This is the codebase for my personal portfolio. Below are the instructions for setting up and running the project locally, as well as using the code.
+
+## 🚀 Getting Started
+
+### 1 Clone the Repository
+
+````sh
+git clone git@github.com-personal:yuliapsavinkova/yuliapsavinkova.github.io.git
+cd yuliapsavinkova.github.io
+
+```bash
+git clone https://github.com/yuliapsavinkova/yuliapsavinkova.github.io.git
+cd yuliapsavinkova.github.io
+````
+
+2. **Install dependencies:**
+
+npm install
+
+3. **Run the project locally:**
+
+npm run dev
+
+This will start the development server, and you can view the portfolio by opening http://localhost:3000 in your browser.
--- src/portfolio/components/particles.js
+++ src/portfolio/components/particles.js
@@ -1,22 +1,22 @@
 export const particlesConfig = {
   particles: {
     number: { value: 5, density: { enable: true, value_area: 800 } },
-    color: { value: "#2d4a77" },
+    color: { value: "#e2ebf6" },
     shape: {
       type: "polygon",
       stroke: { width: 0, color: "#000" },
-      polygon: { nb_sides: 6 },
+      polygon: { nb_sides: 10 },
       // image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
-      value: 0.1,
+      value: 0.5,
       random: true,
       // anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
     },
     size: {
-      value: 200,
+      value: 150,
       random: false,
-      anim: { enable: true, speed: 20, size_min: 100, sync: false },
+      anim: { enable: true, speed: 20, size_min: 50, sync: false },
     },
     // line_linked: {
     //   enable: false,
@@ -27,7 +27,7 @@ export const particlesConfig = {
     // },
     move: {
       enable: true,
-      speed: 8,
+      speed: 10,
       direction: "none",
       random: false,
       straight: false,
