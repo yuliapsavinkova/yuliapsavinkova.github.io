dfbcbda5f9a09883a2f0c102cdf8d12e57fa4550
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-07-31 16:11:13 -0500

    expertise

--- src/portfolio/components/expertise.js
+++ src/portfolio/components/expertise.js
@@ -9,13 +9,13 @@ class ExpertiseComponent extends HTMLElement {
 
             <div class="card-grid">
 
-                <div class="card bg-texture">
-                    <div class="card-image">
+                <div class="card">
+                    <div class="card-image bg-texture">
                       <img src="./images/web-dev.png" loading="lazy" alt="maintenance-image" />
                     </div>
                     <div class="card-content">
-                      <h2>Web Development</h2>
-                      <p>Build websites from scratch using HTML, CSS, JavaScript, and Web Components.</p>
+                      <h2>Custom Websites</h2>
+                      <p>I build custom websites for portfolios, freelancers, and small businesses</p>
                       <a href="#/work?section=work-row-web" aria-label="Go to Web Development section">
                         <svg class="icon icon-stroke enable-icon-scale" aria-hidden="true">
                           <use href="#icon-arrow-right"></use>
@@ -24,13 +24,13 @@ class ExpertiseComponent extends HTMLElement {
                     </div>
                 </div>
 
-                <div class="card bg-texture">
-                    <div class="card-image">
+                <div class="card">
+                    <div class="card-image bg-texture">
                       <img src="./images/app-dev.png" class="card-image" loading="lazy" alt="web-development-image" />            
                     </div>
                     <div class="card-content">
-                      <h2>App Development</h2>
-                      <p>Build web applications using modern frameworks such as React and Angular.</p>
+                      <h2>Web Applications</h2>
+                      <p>I develop web applications with user logins, forms, and dashboards</p>
                       <a href="#/work?section=work-row-app" aria-label="Go to App Development section">
                         <svg class="icon icon-stroke enable-icon-scale" aria-hidden="true">
                           <use href="#icon-arrow-right"></use>
@@ -39,7 +39,22 @@ class ExpertiseComponent extends HTMLElement {
                     </div>
                 </div>
 
-                <div class="card bg-texture">
+                <div class="card">
+                    <div class="card-image bg-texture">
+                      <img src="./images/fin-tools.png" class="card-image" loading="lazy" alt="web-development-image" />            
+                    </div>
+                    <div class="card-content">
+                      <h2>Financial Tools</h2>
+                      <p>I create financial tools that help traders and investors make financial decisions</p>
+                      <a href="#/work?section=work-row-app" aria-label="Go to App Development section">
+                        <svg class="icon icon-stroke enable-icon-scale" aria-hidden="true">
+                          <use href="#icon-arrow-right"></use>
+                        </svg>
+                      </a>
+                    </div>
+                </div>
+
+                <!--<div class="card bg-texture">
                     <div class="card-image">
                       <img src="./images/three-d-dev.png" class="card-image" loading="lazy" alt="design-image" />
                     </div>
@@ -52,7 +67,7 @@ class ExpertiseComponent extends HTMLElement {
                         </svg>
                       </a>
                     </div>
-                </div>
+                </div>-->
 
             </div>
             
