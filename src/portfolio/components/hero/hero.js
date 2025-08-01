7fb8a139ffd542be52c8b7159db0770ddd480eed
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-08-01 17:52:59 -0500

    lazy load

--- src/portfolio/components/hero/hero.js
+++ src/portfolio/components/hero/hero.js
@@ -7,7 +7,7 @@ class HeroComponent extends HTMLElement {
       <section id="hero" class="hero bg-texture">
         <div class="hero-container">
           <div>
-            <img src="./images/hero.png" alt="hero image" class="hero-image">
+            <img src="./images/hero.png" alt="hero image" class="hero-image" loading="lazy">
           </div>
           <div class="hero-heading">
             <h1>Hello, I'm Yulia</h1>
