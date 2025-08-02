5c138f97abad05b413e5f2a8a9e3cdd5344c2f0e
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-08-01 20:28:55 -0500

    webp

--- src/portfolio/components/about.js
+++ src/portfolio/components/about.js
@@ -10,7 +10,7 @@ class AboutComponent extends HTMLElement {
 
                 <div class="about-me feature-card">
                     <div class="card-image">
-                        <img src="./images/portrait.png" loading="lazy" alt="Head shot of Yulia Savinkova, a web developer."/>
+                        <img src="./images/portrait.webp" loading="lazy" alt="Head shot of Yulia Savinkova, a web developer."/>
                     </div>
                     <div class="card-content">
                         <h2>Code & Beyond</h2>
