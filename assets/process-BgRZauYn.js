f1757e6d487d872ee2a77515c05434c6e8c1dd59
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2025-08-02 01:57:01 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@01d0d34a97569b266389b898124abae31fed7dfb 🚀

--- assets/process-BgRZauYn.js
+++ assets/process-BgRZauYn.js
@@ -1,5 +1,5 @@
 class e extends HTMLElement{connectedCallback(){this.innerHTML=`
-      <section id="working-process" class="section working-process bg-working-girl">
+      <section id="working-process" class="section working-process">
         <section-header
           title="Working Process"
           sub-title="Collaborate, Design, Code, and Optimize for Fast, Responsive Web Experiences.">
