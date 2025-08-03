27101b69173ff1025e54dc3fc81407f43a6f7c6f
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-08-02 21:08:54 -0500

    css

--- src/components/palette/palette.js
+++ src/components/palette/palette.js
@@ -12,13 +12,10 @@ class Palette extends HTMLElement {
                 <h1>Colors</h1>
                 <div class="circle primary-dark-circle"></div>
                 <div class="circle primary-light-circle"></div>
-                <div class="circle neutral-dark-circle"></div>
-                <div class="circle neutral-light-circle"></div>
+                <div class="circle text-circle"></div>
                 <div class="circle accent-dark-circle"></div>
                 <div class="circle accent-light-circle"></div>
-                <div class="circle text-circle"></div>
                 <div class="circle white-circle"></div>
-                <div class="circle grey-circle"></div>
                 <div class="circle black-circle"></div>
             </div>
             <div class="palette-buttons">
@@ -26,8 +23,6 @@ class Palette extends HTMLElement {
                 <button class="button button-primary">Primary</button>
                 <button class="button button-secondary">Secondary</button>
                 <button class="button button-action">Action</button>
-                <button class="button button-tertiary">Tertiary</button>
-                <a href="#" class="button button-link">Button Link</a>
             </div>
         </section>
         `;
