83ebb0af7c34d3cbbf8977d75c398ce514e404c6
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-08-08 20:21:33 -0500

    css code split

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: 'index.html',
     },
     minify: 'esbuild',
-    cssCodeSplit: false,
+    cssCodeSplit: true,
     // sourcemap: true,
   },
   server: {
@@ -35,5 +35,38 @@ export default defineConfig({
         ],
       },
     }),
+    {
+      name: 'html-preload-css',
+      apply: 'build',
+      enforce: 'post',
+      generateBundle(options, bundle) {
+        const htmlFileName = 'index.html';
+        const htmlChunk = bundle[htmlFileName];
+        if (!htmlChunk) {
+          this.error(`Could not find ${htmlFileName} in bundle`);
+          return;
+        }
+
+        // Find all CSS files in the bundle
+        const cssFiles = Object.values(bundle)
+          .filter((f) => f.type === 'asset' && f.fileName.endsWith('.css'))
+          .map((f) => f.fileName);
+
+        let htmlSource = htmlChunk.source;
+
+        // Build preload links string
+        const preloadLinks = cssFiles
+          .map((file) => {
+            return `<link rel="preload" href="${file}" as="style" crossorigin="anonymous">`;
+          })
+          .join('\n');
+
+        // Inject preload links into <head> before existing stylesheets
+        htmlSource = htmlSource.replace(/(<\/head>)/i, preloadLinks + '\n$1');
+
+        // Replace the html chunk source
+        htmlChunk.source = htmlSource;
+      },
+    },
   ],
 });
