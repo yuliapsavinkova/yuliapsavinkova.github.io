6393fd5366d8e4630f38029d39cb2e3fd38c7840
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-12-01 13:05:56 -0800

    different sizes of image optimization

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,15 @@ class AboutComponent extends HTMLElement {
 
                 <div class="about-me feature-card">
                     <div class="card-image">
-                        <img src="./images/portrait.webp" loading="lazy" alt="Head shot of Yulia Savinkova, a web developer."/>
+                        <img 
+                            src="./images/portrait-800.webp" 
+                            srcset="./images/portrait-400.webp 400w, 
+                                    ./images/portrait-800.webp 800w, 
+                                    ./images/portrait-1200.webp 1200w"
+                            sizes="(max-width: 500px) 95vw, 480px"
+                            loading="lazy" 
+                            alt="Head shot of Yulia Savinkova, a web developer."
+                        />
                     </div>
                     <div class="card-content">
                         <h2>Code & Beyond</h2>
