8de8569262319591c21c1cc4106630309cfff018
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2025-12-03 02:13:32 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@ee17b9446dcc96d9a6483b529c760afc7434f807 🚀

--- assets/hero-D2wcotb0.js
+++ assets/hero-D2wcotb0.js
@@ -64,19 +64,20 @@ class r extends HTMLElement{constructor(){super(),this.titles=[],this.titleIndex
           height: 50%;
           aspect-ratio: 1 / 1;
           object-fit: contain;
-          border-radius: var(--border-radius);
+          border-radius: 50%;
         }
       </style>
       <section id="hero" class="hero bg-texture">
-        <img src="./images/portrait-800.webp"
-              srcset="./images/portrait-400.webp 400w, 
-                      ./images/portrait-800.webp 800w, 
-                      ./images/portrait-1200.webp 1200w"
-              sizes="(max-width: 600px) 90vw, 50vh"
-              alt="Portrait of Yulia"
-              class="hero-image-portrait"
-              fetchpriority="high"
-              decoding="async">
+        <img src="./images/portrait-900.webp"
+            srcset="./images/portrait-400.webp 400w, 
+                    ./images/portrait-600.webp 600w, 
+                    ./images/portrait-900.webp 900w, 
+                    ./images/portrait-1200.webp 1200w"
+            sizes="(max-width: 600px) 90vw, 50vh"
+            alt="Portrait of Yulia"
+            class="hero-image-portrait"
+            fetchpriority="high"
+            decoding="async">
         <div class="hero-heading">
           <h1>Hello, I'm Yulia</h1>
           <rotating-text data-titles='[
@@ -86,8 +87,8 @@ class r extends HTMLElement{constructor(){super(),this.titles=[],this.titleIndex
           ]'></rotating-text>
         </div>
         <div class="hero-buttons">
-          <a href="#/about" class="button button-primary">Learn More</a>
-          <a href="#/contact" class="button button-action">Contact</a>
+          <a href="#about" class="button button-primary">Learn More</a>
+          <a href="#contact" class="button button-action">Contact</a>
         </div>
       </section>
     `}}customElements.define("hero-component",a);
