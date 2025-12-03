ee17b9446dcc96d9a6483b529c760afc7434f807
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-12-02 18:13:03 -0800

    portrait

--- src/components/hero.js
+++ src/components/hero.js
@@ -31,19 +31,20 @@ class HeroComponent extends HTMLElement {
           height: 50%;
           aspect-ratio: 1 / 1;
           object-fit: contain;
-          border-radius: var(--border-radius);
+          border-radius: 50%;
         }
       </style>
       <section id="hero" class="hero bg-texture">
-        <img src="./images/portrait-800.webp"
-              srcset="./images/portrait-400.webp 400w, 
-                      ./images/portrait-800.webp 800w, 
-                      ./images/portrait-1200.webp 1200w"
-              sizes="(max-width: 600px) 90vw, 50vh"
-              alt="Portrait of Yulia"
-              class="hero-image-portrait"
-              fetchpriority="high"
-              decoding="async">
+        <img src="./images/portrait-900.webp"
+            srcset="./images/portrait-400.webp 400w, 
+                    ./images/portrait-600.webp 600w, 
+                    ./images/portrait-900.webp 900w, 
+                    ./images/portrait-1200.webp 1200w"
+            sizes="(max-width: 600px) 90vw, 50vh"
+            alt="Portrait of Yulia"
+            class="hero-image-portrait"
+            fetchpriority="high"
+            decoding="async">
         <div class="hero-heading">
           <h1>Hello, I'm Yulia</h1>
           <rotating-text data-titles='[
@@ -53,8 +54,8 @@ class HeroComponent extends HTMLElement {
           ]'></rotating-text>
         </div>
         <div class="hero-buttons">
-          <a href="#/about" class="button button-primary">Learn More</a>
-          <a href="#/contact" class="button button-action">Contact</a>
+          <a href="#about" class="button button-primary">Learn More</a>
+          <a href="#contact" class="button button-action">Contact</a>
         </div>
       </section>
     `;
