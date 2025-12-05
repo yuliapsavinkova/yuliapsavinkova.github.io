1cdef7076378220c24316a4bd1c2488263eabce8
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2025-12-05 17:03:48 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@9eb2a860d43180fc1fe9dc8bca1a0343aeece1f1 🚀

---
+++ assets/process-BY3CEUxx.js
@@ -0,0 +1,60 @@
+class e extends HTMLElement{connectedCallback(){this.innerHTML=`
+      <style>
+        .working-process {
+          background: linear-gradient(var(--color-overlay), var(--color-overlay)),
+            url('../../images/working-girl-480.webp');
+            
+          background-size: cover;
+          background-repeat: no-repeat;
+          /* Default to SCROLL for optimal mobile performance */
+          background-attachment: scroll; 
+          background-position: center center;
+          
+          height: 30rem;
+          width: auto;
+
+          * {
+            color: var(--white);
+          }
+        }
+          
+        /* Tablet size and up (Min-width: 768px) */
+        @media (min-width: 768px) {
+          .working-process {
+            background-image: linear-gradient(var(--color-overlay), var(--color-overlay)),
+              url('../../images/working-girl-768.webp');
+            /* Enable the parallax effect for tablets, laptops, and desktops */
+            background-attachment: fixed;
+          }
+        }
+
+        /* Laptop size and up (Min-width: 1280px) */
+        @media (min-width: 1280px) {
+          .working-process {
+            background-image: linear-gradient(var(--color-overlay), var(--color-overlay)),
+              url('../../images/working-girl-1280.webp');
+          }
+        }
+
+        /* Large desktop size and up (Min-width: 1920px) */
+        @media (min-width: 1920px) {
+          .working-process {
+            background-image: linear-gradient(var(--color-overlay), var(--color-overlay)),
+              url('../../images/working-girl-1920.webp');
+          }
+        }
+        
+        /* Ultra-wide Screen Height Fix */
+        @media (min-width: 2560px) {
+          .working-process {
+            height: 40rem; 
+          }
+        }
+      </style>
+      <section id="working-process" class="working-process">
+        <section-header
+          title="Working Process"
+          sub-title="Collaborate, Design, Code, and Optimize for Fast, Responsive Web Experiences.">
+        </section-header>
+      </section>
+    `}}customElements.define("working-process-component",e);
