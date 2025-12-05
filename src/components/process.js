9eb2a860d43180fc1fe9dc8bca1a0343aeece1f1
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-12-05 09:03:20 -0800

    screens

--- src/components/process.js
+++ src/components/process.js
@@ -4,23 +4,52 @@ class WorkingProcessComponent extends HTMLElement {
       <style>
         .working-process {
           background: linear-gradient(var(--color-overlay), var(--color-overlay)),
-            url('../../images/working-girl-1280.webp');
+            url('../../images/working-girl-480.webp');
+            
           background-size: cover;
           background-repeat: no-repeat;
+          /* Default to SCROLL for optimal mobile performance */
+          background-attachment: scroll; 
           background-position: center center;
-          background-attachment: fixed;
-          height: 25rem;
+          
+          height: 30rem;
           width: auto;
 
           * {
             color: var(--white);
           }
         }
-
-        @media (max-width: 1400px) {
+          
+        /* Tablet size and up (Min-width: 768px) */
+        @media (min-width: 768px) {
           .working-process {
-            background-attachment: scroll;
-            background-position: left center;
+            background-image: linear-gradient(var(--color-overlay), var(--color-overlay)),
+              url('../../images/working-girl-768.webp');
+            /* Enable the parallax effect for tablets, laptops, and desktops */
+            background-attachment: fixed;
+          }
+        }
+
+        /* Laptop size and up (Min-width: 1280px) */
+        @media (min-width: 1280px) {
+          .working-process {
+            background-image: linear-gradient(var(--color-overlay), var(--color-overlay)),
+              url('../../images/working-girl-1280.webp');
+          }
+        }
+
+        /* Large desktop size and up (Min-width: 1920px) */
+        @media (min-width: 1920px) {
+          .working-process {
+            background-image: linear-gradient(var(--color-overlay), var(--color-overlay)),
+              url('../../images/working-girl-1920.webp');
+          }
+        }
+        
+        /* Ultra-wide Screen Height Fix */
+        @media (min-width: 2560px) {
+          .working-process {
+            height: 40rem; 
           }
         }
       </style>
