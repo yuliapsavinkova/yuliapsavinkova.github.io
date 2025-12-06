a9395c468b3cdccdf6571494d22da355db4034dd
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2025-12-06 02:27:03 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@1d06850c89a65623abc2274259fadae033493aef 🚀

--- assets/hero-7HihpZPn.js
+++ assets/hero-7HihpZPn.js
@@ -1,4 +1,4 @@
-import"./social-icons-D5rXZ1el.js";class n extends HTMLElement{constructor(){super(),this.titles=[],this.titleIndex=0,this.charIndex=0,this.isDeleting=!1,this.lastFrameTime=0,this.typeSpeed=100,this.deleteSpeed=50}connectedCallback(){this.render(),this.init()}render(){this.innerHTML=`
+import"./social-icons-D5rXZ1el.js";class r extends HTMLElement{constructor(){super(),this.titles=[],this.titleIndex=0,this.charIndex=0,this.isDeleting=!1,this.lastFrameTime=0,this.typeSpeed=100,this.deleteSpeed=50}connectedCallback(){this.render(),this.init()}render(){this.innerHTML=`
         <style>
           rotating-text {
             display: inline-block;
@@ -35,7 +35,7 @@ import"./social-icons-D5rXZ1el.js";class n extends HTMLElement{constructor(){sup
             <span class="typewriter-text"></span>
             <span class="cursor">|</span>
         </div>
-    `}init(){this.textElement=this.querySelector(".typewriter-text");try{this.titles=JSON.parse(this.getAttribute("data-titles"))||[]}catch(t){console.error("Invalid JSON for data-titles attribute on rotating-text component:",t),this.titles=[]}this.titles.length>0&&(this.type=this.type.bind(this),requestAnimationFrame(this.type))}type(t){if(!this.textElement)return;this.lastFrameTime||(this.lastFrameTime=t);const s=t-this.lastFrameTime,r=this.isDeleting?this.deleteSpeed:this.typeSpeed;if(s>r){this.lastFrameTime=t;const e=this.titles[this.titleIndex];this.isDeleting?(this.textElement.textContent=e.substring(0,this.charIndex--),this.charIndex<0&&(this.isDeleting=!1,this.titleIndex=(this.titleIndex+1)%this.titles.length)):(this.textElement.textContent=e.substring(0,this.charIndex++),this.charIndex>e.length&&setTimeout(()=>{this.isDeleting=!0},1e3))}requestAnimationFrame(this.type)}}customElements.define("rotating-text",n);class a extends HTMLElement{connectedCallback(){this.innerHTML=`
+    `}init(){this.textElement=this.querySelector(".typewriter-text");try{this.titles=JSON.parse(this.getAttribute("data-titles"))||[]}catch(t){console.error("Invalid JSON for data-titles attribute on rotating-text component:",t),this.titles=[]}this.titles.length>0&&(this.type=this.type.bind(this),requestAnimationFrame(this.type))}type(t){if(!this.textElement)return;this.lastFrameTime||(this.lastFrameTime=t);const s=t-this.lastFrameTime,n=this.isDeleting?this.deleteSpeed:this.typeSpeed;if(s>n){this.lastFrameTime=t;const e=this.titles[this.titleIndex];this.isDeleting?(this.textElement.textContent=e.substring(0,this.charIndex--),this.charIndex<0&&(this.isDeleting=!1,this.titleIndex=(this.titleIndex+1)%this.titles.length)):(this.textElement.textContent=e.substring(0,this.charIndex++),this.charIndex>e.length&&setTimeout(()=>{this.isDeleting=!0},1e3))}requestAnimationFrame(this.type)}}customElements.define("rotating-text",r);class a extends HTMLElement{connectedCallback(){this.innerHTML=`
       <style>
         .hero {
           width: 100%;
@@ -44,11 +44,41 @@ import"./social-icons-D5rXZ1el.js";class n extends HTMLElement{constructor(){sup
           height: auto;
           display: flex;
           flex-direction: column;
-          justify-content: center;
+          justify-content: flex-end; 
           align-items: center;
           gap: var(--gap-small);
           padding: 0;
-          background-color: var(--color-background);
+          position: relative; 
+          padding-bottom: var(--gap-large); 
+        }
+
+        .hero-banner {
+          background-color: var(--color-primary); 
+          overflow: hidden; 
+          position: absolute; 
+          top: 0;
+          left: 0;
+          right: 0;
+          height: 50%;
+          z-index: -1; 
+        }
+
+        .hero-banner::before {
+          content: '';
+          position: absolute;
+          inset: 0;
+          background: url('/images/banner-bg.png') no-repeat;
+          background-size: cover;
+          opacity: 0.9;
+          mix-blend-mode: luminosity; 
+          pointer-events: none;
+        }
+        
+        .hero-image-portrait {
+          height: 50%;
+          aspect-ratio: 1 / 1;
+          border-radius: var(--border-radius-lg);
+          border: 4px solid var(--white)
         }
 
         .hero-heading {
@@ -61,14 +91,9 @@ import"./social-icons-D5rXZ1el.js";class n extends HTMLElement{constructor(){sup
           justify-content: center;
           gap: var(--gap-tiny);
         }
-
-        .hero-image-portrait {
-          height: 50%;
-          aspect-ratio: 1 / 1;
-          border-radius: var(--border-radius-lg);
-        }
       </style>
       <section id="hero" class="hero">
+        <div class="hero-banner"></div> 
         <img src="./images/portrait-900.webp"
             srcset="./images/portrait-400.webp 400w, 
                     ./images/portrait-600.webp 600w, 
