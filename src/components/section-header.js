48ca50ad8e3d1b319a4fa7a6745dfd6bf5b305a8
Yulia Savinkova <yuliasavinkova@gmail.com>
2025-12-12 11:01:29 -0800

    separator

--- src/components/section-header.js
+++ src/components/section-header.js
@@ -16,10 +16,14 @@ class SectionHeader extends HTMLElement {
           }
 
           .gra-separator {
-            border-top-width: 2px;
-            border-top-style: solid;
-            width: 6rem;
-            border-top-color: var(--color-accent);
+            height: 2px;
+            width: 20rem;
+            background: linear-gradient(to right, 
+              var(--color-background) 0%, 
+              var(--color-accent) 50%, 
+              var(--color-background) 100%
+            );
+            
           }
         }
       </style>
