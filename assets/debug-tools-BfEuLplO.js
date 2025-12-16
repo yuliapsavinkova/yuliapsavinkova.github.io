3bf66d612197853d23a6d46d5d73af89d03187d2
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2025-12-16 22:25:32 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@8b508d2f88253f3161a396c85e64285727de1614 🚀

--- assets/debug-tools-BfEuLplO.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/debug-panel-BuEtC7VY.js","assets/index-DGIqO7SV.js","assets/theme-toggle-9PDmT22X.js"])))=>i.map(i=>d[i]);
-import{_ as n}from"./index-DGIqO7SV.js";async function t(){if(document.getElementById("debug-panel"))return;console.log("Debug mode active"),await n(()=>import("./debug-panel-BuEtC7VY.js"),__vite__mapDeps([0,1,2]));const e=document.createElement("div");e.id="debug-panel",e.innerHTML="<debug-panel-component></debug-panel-component>",document.body.appendChild(e)}export{t as loadDebugTools};
