1aea8ad3019d6182f97f2c46249aeae268f80aa4
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2025-12-30 02:59:04 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@3ae0c04dd64a5c26c8aaa93dc87ee9e28ab0ee9b 🚀

---
+++ assets/debug-tools-BHLzvcmS.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/debug-panel-DK1iSL-8.js","assets/index-BdbLFtvE.js","assets/theme-toggle-9PDmT22X.js"])))=>i.map(i=>d[i]);
+import{_ as n}from"./index-BdbLFtvE.js";async function t(){if(document.getElementById("debug-panel"))return;console.log("Debug mode active"),await n(()=>import("./debug-panel-DK1iSL-8.js"),__vite__mapDeps([0,1,2]));const e=document.createElement("div");e.id="debug-panel",e.innerHTML="<debug-panel-component></debug-panel-component>",document.body.appendChild(e)}export{t as loadDebugTools};
