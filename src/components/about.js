d3114f0b27448132b7ed56acfbd9f8652f5a4ab5
Yulia Savinkova <yuliasavinkova@gmail.com>
2026-03-01 15:15:35 -0800

    resume link

--- src/components/about.js
+++ src/components/about.js
@@ -39,7 +39,7 @@ class AboutComponent extends HTMLElement {
                             I stay current on how AI is transforming engineering and integrate it into my workflow to improve productivity and efficiency. Outside of work, I enjoy reading, traveling, and spending time with animals.
                         </p>
                         <div>
-                            <a href="./resume.pdf" target="_blank" class="button button-primary">Resume</a>
+                            <a href="./resume-full.pdf" target="_blank" class="button button-primary">Resume</a>
                             <a href="#/about?section=work-experience" class="button button-secondary">Learn More</a>
                         </div>
                     </div>
