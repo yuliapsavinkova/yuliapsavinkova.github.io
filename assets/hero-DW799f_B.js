511763f78a2c42bbfc578b7847536e30fb6f2491
yuliapsavinkova <61819360+yuliapsavinkova@users.noreply.github.com>
2026-03-11 15:07:50 +0000

    Deploying to gh-pages from @ yuliapsavinkova/yuliapsavinkova.github.io@9fe86cef16ccc26df872264c246341856cdff8f5 🚀

--- assets/hero-DW799f_B.js
+++ assets/hero-DW799f_B.js
@@ -35,7 +35,7 @@ import"./social-icons-D5rXZ1el.js";class a extends HTMLElement{constructor(){sup
             <span class="typewriter-text"></span>
             <span class="cursor">|</span>
         </div>
-    `}init(){this.textElement=this.querySelector(".typewriter-text");try{this.titles=JSON.parse(this.getAttribute("data-titles"))||[]}catch(e){console.error("Invalid JSON for data-titles attribute on rotating-text component:",e),this.titles=[]}this.titles.length>0&&(this.type=this.type.bind(this),requestAnimationFrame(this.type))}type(e){if(!this.textElement)return;this.lastFrameTime||(this.lastFrameTime=e);const r=e-this.lastFrameTime,n=this.isDeleting?this.deleteSpeed:this.typeSpeed;if(r>n){this.lastFrameTime=e;const t=this.titles[this.titleIndex];this.isDeleting?(this.textElement.textContent=t.substring(0,this.charIndex--),this.charIndex<0&&(this.isDeleting=!1,this.titleIndex=(this.titleIndex+1)%this.titles.length)):(this.textElement.textContent=t.substring(0,this.charIndex++),this.charIndex>t.length&&setTimeout(()=>{this.isDeleting=!0},1e3))}requestAnimationFrame(this.type)}}customElements.define("rotating-text",a);class s extends HTMLElement{connectedCallback(){this.innerHTML=`
+    `}init(){this.textElement=this.querySelector(".typewriter-text");try{this.titles=JSON.parse(this.getAttribute("data-titles"))||[]}catch(t){console.error("Invalid JSON for data-titles attribute on rotating-text component:",t),this.titles=[]}this.titles.length>0&&(this.type=this.type.bind(this),requestAnimationFrame(this.type))}type(t){if(!this.textElement)return;this.lastFrameTime||(this.lastFrameTime=t);const s=t-this.lastFrameTime,r=this.isDeleting?this.deleteSpeed:this.typeSpeed;if(s>r){this.lastFrameTime=t;const e=this.titles[this.titleIndex];this.isDeleting?(this.textElement.textContent=e.substring(0,this.charIndex--),this.charIndex<0&&(this.isDeleting=!1,this.titleIndex=(this.titleIndex+1)%this.titles.length)):(this.textElement.textContent=e.substring(0,this.charIndex++),this.charIndex>e.length&&setTimeout(()=>{this.isDeleting=!0},1e3))}requestAnimationFrame(this.type)}}customElements.define("rotating-text",a);class n extends HTMLElement{connectedCallback(){this.innerHTML=`
       <style>
         .hero {
           width: 100%;
@@ -47,32 +47,6 @@ import"./social-icons-D5rXZ1el.js";class a extends HTMLElement{constructor(){sup
           align-items: center;
           gap: var(--gap-small);
           padding: 0 var(--padding-inline);
-        }
-
-        .hero-banner, .hero-banner::before {
-            border-radius: var(--border-radius-lg);
-        }
-        .hero-banner {
-            display: block;
-            background-color: var(--color-primary); 
-            position: absolute; 
-            top: 3rem;
-            left: 0;
-            right: 0;
-            height: 50%;
-            z-index: -1; 
-        }
-
-        .hero-banner::before {
-            content: '';
-            position: absolute;
-            inset: 0;
-            background-image: url('/images/banner-bg-mobile.webp'); /* Mobile (Vertical) Image */
-            background-size: cover;
-            background-repeat: no-repeat;
-            opacity: 0.9;
-            mix-blend-mode: luminosity; 
-            pointer-events: none;
         }
 
         .hero-image-portrait {
@@ -100,24 +74,9 @@ import"./social-icons-D5rXZ1el.js";class a extends HTMLElement{constructor(){sup
           .hero {
             padding: var(--gap-large) 0;
           }
-
-          .hero-banner {
-            margin: 0 1rem;
-          }
-
-          .hero-banner::before {
-              background-image: url('/images/banner-bg-tablet.webp');
-          }
-        }
-
-        @media (min-width: 1025px) {
-          .hero-banner::before {
-              background-image: url('/images/banner-bg-desktop.webp');
-          }
         }
       </style>
       <section id="hero" class="hero">
-        <div class="hero-banner"></div> 
         <img
           src="./images/portrait-900.webp"
           srcset="
@@ -146,4 +105,4 @@ import"./social-icons-D5rXZ1el.js";class a extends HTMLElement{constructor(){sup
           <a href="#/contact" class="button button-action">Contact</a>
         </div>
       </section>
-    `}}customElements.define("hero-component",s);
+    `}}customElements.define("hero-component",n);
