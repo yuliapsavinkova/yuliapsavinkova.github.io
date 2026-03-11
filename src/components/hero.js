9fe86cef16ccc26df872264c246341856cdff8f5
Yulia Savinkova <yuliasavinkova@gmail.com>
2026-03-10 19:08:13 -0700

    removed banner

--- src/components/hero.js
+++ src/components/hero.js
@@ -17,32 +17,6 @@ class HeroComponent extends HTMLElement {
           padding: 0 var(--padding-inline);
         }
 
-        .hero-banner, .hero-banner::before {
-            border-radius: var(--border-radius-lg);
-        }
-        .hero-banner {
-            display: block;
-            background-color: var(--color-primary); 
-            position: absolute; 
-            top: 3rem;
-            left: 0;
-            right: 0;
-            height: 50%;
-            z-index: -1; 
-        }
-
-        .hero-banner::before {
-            content: '';
-            position: absolute;
-            inset: 0;
-            background-image: url('/images/banner-bg-mobile.webp'); /* Mobile (Vertical) Image */
-            background-size: cover;
-            background-repeat: no-repeat;
-            opacity: 0.9;
-            mix-blend-mode: luminosity; 
-            pointer-events: none;
-        }
-
         .hero-image-portrait {
           width: auto;
           height: 45vh;
@@ -68,24 +42,9 @@ class HeroComponent extends HTMLElement {
           .hero {
             padding: var(--gap-large) 0;
           }
-
-          .hero-banner {
-            margin: 0 1rem;
-          }
-
-          .hero-banner::before {
-              background-image: url('/images/banner-bg-tablet.webp');
-          }
-        }
-
-        @media (min-width: 1025px) {
-          .hero-banner::before {
-              background-image: url('/images/banner-bg-desktop.webp');
-          }
         }
       </style>
       <section id="hero" class="hero">
-        <div class="hero-banner"></div> 
         <img
           src="./images/portrait-900.webp"
           srcset="
